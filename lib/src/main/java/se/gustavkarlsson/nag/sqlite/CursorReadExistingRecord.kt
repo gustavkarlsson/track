bfb6c0b5565c527a452a475f3d603330e9b9bc1b
Gustav Karlsson <gustav.karlsson@gmail.com>
2019-09-09 22:17:42 +0200

    Cleaned up readExistingRecord

--- lib/src/main/java/se/gustavkarlsson/nag/sqlite/CursorReadExistingRecord.kt
+++ lib/src/main/java/se/gustavkarlsson/nag/sqlite/CursorReadExistingRecord.kt
@@ -4,9 +4,23 @@ import android.database.Cursor
 import se.gustavkarlsson.nag.Record
 
 internal fun Cursor.readExistingRecord(): Record = Record(
-	id = getLong(getColumnIndex(Table.COLUMN_ID)),
-	key = getString(getColumnIndex(Table.COLUMN_KEY)),
-	timestamp = getLong(getColumnIndex(Table.COLUMN_TIMESTAMP)),
-	appVersion = getLong(getColumnIndex(Table.COLUMN_APP_VERSION)),
-	value = getString(getColumnIndex(Table.COLUMN_VALUE))
+	id = get(Table.COLUMN_ID),
+	key = get(Table.COLUMN_KEY),
+	timestamp = get(Table.COLUMN_TIMESTAMP),
+	appVersion = get(Table.COLUMN_APP_VERSION),
+	value = get(Table.COLUMN_VALUE)
 )
+
+private inline operator fun <reified T> Cursor.get(column: String): T {
+	val index = getColumnIndexOrThrow(column)
+	return when (T::class) {
+		Short::class -> getShort(index)
+		Int::class -> getInt(index)
+		Long::class -> getLong(index)
+		Float::class -> getFloat(index)
+		Double::class -> getDouble(index)
+		ByteArray::class -> getBlob(index)
+		String::class -> getString(index)
+		else -> throw IllegalArgumentException("Could not read ${T::class} from $column")
+	} as T
+}
