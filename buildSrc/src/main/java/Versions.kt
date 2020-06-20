5630840a3f6824bd3358c26f7bab1759291bf312
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-06-20 23:32:03 +0200

    Upgrade versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,19 +5,19 @@ object Versions {
     val minSdk = 21
     val compileSdk = 29
     val targetSdk = 29
-    val buildTools = "29.0.2"
+    val buildTools = "29.0.3"
     val java = JavaVersion.VERSION_1_8
     val jvmTarget = "1.8"
-    val kotlin = "1.3.50"
+    val kotlin = "1.3.72"
     val supportAnnotations = "28.0.0"
-    val androidGradle = "3.5.0"
-    val dokka = "0.9.18"
-    val ktlint = "9.0.0"
+    val androidGradle = "4.0.0"
+    val dokka = "0.10.1"
+    val ktlint = "9.2.1"
 
     // Test
     val junit = "4.12"
-    val assertk = "0.19"
-    val mockito = "3.0.0"
+    val assertk = "0.22"
+    val mockito = "3.3.3"
     val mockitoKotlin = "2.2.0"
 
     // Instrumentation Test
