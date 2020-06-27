45f8f3ccdae676660f68df62349e9000b0ca3f07
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-06-27 20:01:27 +0200

    Make version numbers constants

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,28 +2,28 @@ import org.gradle.api.JavaVersion
 
 object Versions {
     // Config
-    val minLibSdk = 21
-    val minAppSdk = 26
-    val compileSdk = 29
-    val targetSdk = 29
-    val buildTools = "29.0.3"
+    const val minLibSdk = 21
+    const val minAppSdk = 26
+    const val compileSdk = 29
+    const val targetSdk = 29
+    const val buildTools = "29.0.3"
     val java = JavaVersion.VERSION_1_8
-    val jvmTarget = "1.8"
-    val kotlin = "1.3.72"
-    val supportAnnotations = "28.0.0"
-    val androidGradle = "4.0.0"
-    val dokka = "0.10.1"
-    val ktlint = "9.2.1"
+    const val jvmTarget = "1.8"
+    const val kotlin = "1.3.72"
+    const val supportAnnotations = "28.0.0"
+    const val androidGradle = "4.0.0"
+    const val dokka = "0.10.1"
+    const val ktlint = "9.2.1"
 
     // Test
-    val junit = "4.12"
-    val mockito = "3.3.3"
-    val mockitoKotlin = "2.2.0"
-    val strikt = "0.26.1"
+    const val junit = "4.12"
+    const val mockito = "3.3.3"
+    const val mockitoKotlin = "2.2.0"
+    const val strikt = "0.26.1"
 
     // Instrumentation Test
-    val androidTest = "1.2.0"
+    const val androidTest = "1.2.0"
 
     // Lint
-    val lint = "27.0.0"
+    const val lint = "27.0.0"
 }
